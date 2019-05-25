431ebc0cbc46dfe0aaeb5698e7ac69291f223391
Patrick Amsler <patrick.amsler@zuhlke.com>
2019-05-25 08:05:39 -0700

    initial commit

---
+++ src/main/java/graphql/bookstore/graphqlbookstorecodefirst/GraphqlBookstoreCodefirstApplication.java
@@ -0,0 +1,13 @@
+package graphql.bookstore.graphqlbookstorecodefirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlBookstoreCodefirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlBookstoreCodefirstApplication.class, args);
+	}
+
+}
