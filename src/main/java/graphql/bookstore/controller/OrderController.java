8983286161a99b0bd4100f4dddb484d673a21aa7
Patrick Amsler <patrick.amsler@zuhlke.com>
2019-05-25 09:08:49 -0700

    code clean up

--- src/main/java/graphql/bookstore/controller/OrderController.java
+++ src/main/java/graphql/bookstore/controller/OrderController.java
@@ -1,10 +1,12 @@
 package graphql.bookstore.controller;
 
-import graphql.bookstore.model.Book;
 import graphql.bookstore.model.Order;
 import graphql.bookstore.repository.BookRepository;
 import graphql.bookstore.repository.OrderRepository;
-import io.leangen.graphql.annotations.*;
+import io.leangen.graphql.annotations.GraphQLArgument;
+import io.leangen.graphql.annotations.GraphQLMutation;
+import io.leangen.graphql.annotations.GraphQLNonNull;
+import io.leangen.graphql.annotations.GraphQLQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
